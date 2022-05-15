df2133f30eb35a59cfd7059c03d73473f68cda0a
NoraPDev <padarnori@gmail.com>
2022-05-15 20:48:16 +0000

    Add values and functions

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-var a1 = document.getElementById("a1");
-var a2 = document.getElementById("a2");
-var a3 = document.getElementById("a3");
-var b1 = document.getElementById("b1");
-var b2 = document.getElementById("b2");
-var b3 = document.getElementById("b3");
-var c1 = document.getElementById("c1");
-var c2 = document.getElementById("c2");
-var c3 = document.getElementById("c3");
-
+let a1 = document.getElementById("a1");
+let a2 = document.getElementById("a2");
+let a3 = document.getElementById("a3");
+let b1 = document.getElementById("b1");
+let b2 = document.getElementById("b2");
+let b3 = document.getElementById("b3");
+let c1 = document.getElementById("c1");
+let c2 = document.getElementById("c2");
+let c3 = document.getElementById("c3");
+let playerOne = document.getElementById("player-one");
+let playerTwo = document.getElementById("player-two");
+var actualPlayer = "X";
 
 function resetCells() {
     a1.value = "";
@@ -45,9 +47,15 @@ function checkCells() {
 
 }
 
-function gameInput() {
-    document.getElementById("a1").value = "X";
 
+function gameInput(object) {
+    if (object.value == ""){
+        object.value = actualPlayer;
+        if (actualPlayer == "X") {
+            actualPlayer = "O";
+        } else {
+            actualPlayer = "X";
+        } 
+        checkCells();
+    } 
 }
-
-
