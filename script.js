be2d397214a2b87473f76f1ad171e32e3b6900d4
NoraPDev <padarnori@gmail.com>
2022-05-31 20:41:17 +0000

    Add more to resetCells function

--- script.js
+++ script.js
@@ -83,6 +83,8 @@ function resetCells() {
     c3.value = "";
     board = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
     actualPlayer = "X";    
+    matchlist = "";
+    counter = "";
 }
 
 function startGame() {
